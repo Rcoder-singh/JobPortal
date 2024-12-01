393b1d4dba7c812857c67426046d1dee275b21da
Rcoder-singh <rishabh22.singh@gmail.com>
2024-12-01 22:37:48 +0530

    Created Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <>Start here</>;
-}
+import Home from "./Pages/Student/Home";
+import Login from "./Pages/Student/Login";
+import Signup from "./Pages/Student/Signup";
+import CompanySignup from "./Pages/Company/CompanySignup";
+import CompanyDashboard from "./Pages/Company/CompanyDashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/CompanySignup" element={<CompanySignup />} />
+          <Route path="/CompanyDashboard" element={<CompanyDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
